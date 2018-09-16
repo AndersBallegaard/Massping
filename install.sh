9f3e68e92aa60d1d680b40bd07ed6a9bbd25324b
AndersBallegaard <anderstb@hotmail.dk>
2018-09-16 12:26:41 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#Very simple system wide install for Massping 
+
+#Right now it only works on ubuntu/debian based distros
+
+#go to tmp working dir
+cd /tmp
+
+
+#update distro
+echo Updating distro
+sudo apt-get update -qy
+
+#install python3
+echo Installing python3
+echo Python3.6+ required
+sudo apt-get install -y python3
+
+
+#download Massping
+echo Installing massping
+sudo apt-get install git -y
+git clone https://github.com/AndersBallegaard/Massping.git
+sudo mv Massping/massping.py /usr/local/bin/massping
+
